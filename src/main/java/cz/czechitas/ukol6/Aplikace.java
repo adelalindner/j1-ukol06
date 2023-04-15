fca6febba065443f16e689db4b43c53e9e286b04
AdelaLindner <a.lindnerova@seznam.cz>
2023-04-15 11:41:10 +0200

    Meziuložení

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -6,6 +6,7 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Aplikace extends JFrame {
 
@@ -30,9 +31,60 @@ public class Aplikace extends JFrame {
         setLayout(new MigLayout("wrap 2", "[right]rel[50:120:150,grow,fill]"));
         setMinimumSize(new Dimension(250, 200));
 
-        //TODO implementovat formulář podle zadání
+        //Husy:
+        JLabel husyLabel = new JLabel("Husy");
+        add(husyLabel);
 
+        JTextField husyField = new JTextField();
+        add(husyField);
+
+        //Králici:
+        JLabel kraliciLabel = new JLabel("Králíci");
+        add(kraliciLabel);
+
+        JTextField kraliciField = new JTextField();
+        add(kraliciField);
+
+        JLabel mezeraPredVypocitat = new JLabel(" ");
+        add(mezeraPredVypocitat);
+
+        //Vypočítat:
+
+        JButton vypocitatButton = new JButton("Vypočítat");
+        add(vypocitatButton);
+
+
+        //Výsledek hlavy:
+        JLabel hlavyLabel = new JLabel("Počet hlav");
+        add(hlavyLabel);
+
+        JTextField hlavyField = new JTextField();
+        add(hlavyField);
+        hlavyField.setEditable(false);
+
+
+        //Výsledek nohy:
+        JLabel nohyLabel = new JLabel("Počet nohou");
+        add(nohyLabel);
+
+        JTextField nohyField = new JTextField();
+        add(nohyField);
+        nohyField.setEditable(false);
+
+        vypocitatButton.addActionListener(e ->
+
+        int pocetHlav = Integer.parseInt(husyField.getText()) + Integer.parseInt(kraliciField.getText());
+
+        hlavyField.setText(Integer.toString(pocetHlav));
+
+        int pocetNohou = Integer.parseInt(husyField.getText()) * 2 + Integer.parseInt(kraliciField.getText()) * 4;
+
+        nohyField.setText((Integer.toString(pocetNohou)));
+        );
         pack();
+
+
     }
+
 
 }
